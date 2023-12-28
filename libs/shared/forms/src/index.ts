27933719149d33024ddd835befa435ae8f1eb9b9
Will Davis <wallace41290@gmail.com>
2023-12-28 09:43:17 -0600

    feat(shared-forms): add implementation

--- libs/shared/forms/src/index.ts
+++ libs/shared/forms/src/index.ts
@@ -1,1 +1,8 @@
-export * from './lib/shared-forms/shared-forms.component';
+export { ListErrorsComponent } from './lib/list-errors/list-errors.component';
+export { DynamicFormComponent } from './lib/dynamic-form/dynamic-form.component';
+export * from './lib/+state/forms.actions';
+export * from './lib/+state/forms.reducer';
+export * from './lib/+state/forms.selectors';
+export * from './lib/+state/forms.interfaces';
+
+export * as ngrxFormsEffects from './lib/+state/forms.effects';
---
+++ libs/shared/forms/src/lib/list-errors/list-errors.component.ts
@@ -0,0 +1,41 @@
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  OnDestroy,
+  OnInit,
+  inject,
+} from '@angular/core';
+import { Store } from '@ngrx/store';
+import { ngrxFormsQuery } from '../+state/forms.selectors';
+import { formsActions } from '../..';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'tk-list-errors',
+  standalone: true,
+  templateUrl: './list-errors.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ListErrorsComponent implements OnInit, OnDestroy {
+  private readonly store = inject(Store);
+  private readonly changeDetectorRef = inject(ChangeDetectorRef);
+  private readonly destroyRef = inject(DestroyRef);
+
+  errors: string[] = [];
+
+  ngOnInit() {
+    this.store
+      .select(ngrxFormsQuery.selectErrors)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((e) => {
+        this.errors = Object.keys(e || {}).map((key) => `${key} ${e[key]}`);
+        this.changeDetectorRef.markForCheck();
+      });
+  }
+
+  ngOnDestroy() {
+    this.store.dispatch(formsActions.initializeErrors());
+  }
+}
