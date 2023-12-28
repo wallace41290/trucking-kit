f8994bb1248ff449ab7cb7c15e3b37207f88109d
Will Davis <wallace41290@gmail.com>
2023-12-28 09:47:35 -0600

    feat(shared-error-handler): add implementation

--- libs/shared/error-handler/src/index.ts
+++ libs/shared/error-handler/src/index.ts
@@ -1,1 +1,5 @@
-export * from './lib/shared-error-handler/shared-error-handler.component';
+export * from './lib/error-handler-interceptor.service';
+export * from './lib/+state/error-handler.reducer';
+export * from './lib/+state/error-handler.actions';
+
+export * as errorHandlerEffects from './lib/+state/error-handler.effects';
