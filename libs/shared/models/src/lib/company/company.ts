0fc8daa60c781debf43b0ef4e993c458a9eb0fbe
Nick Richards <nm.richards@yahoo.com>
2023-12-28 21:09:06 -0500

    feat: add company and driver models

---
+++ libs/shared/models/src/lib/company/company.ts
@@ -0,0 +1,11 @@
+export interface Company {
+  city: string;
+  companyName: string;
+  dotNumber: string;
+  emailAddress: string;
+  mcNumber: string;
+  phoneNumber: number;
+  state: string;
+  streetAddress: string;
+  zipCode: number;
+}
---
+++ libs/shared/models/src/lib/driver/driver.ts
@@ -0,0 +1,9 @@
+export interface Driver {
+  cdlNumber: string;
+  dateOfBirth: Date;
+  emailAddress: string;
+  isActive: boolean;
+  name: string;
+  phoneNumber: number;
+  state: string;
+}
