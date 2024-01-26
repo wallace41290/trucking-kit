d149f96e50b51e656748ae7f8e1b4a839e5a727d
Will Davis <wallace41290@gmail.com>
2024-01-26 14:59:04 -0500

    chore: scaffold products

---
+++ libs/trucking-kit/products/feature-products/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { FeatureProductsComponent } from './feature-products.component';
+
+export const featureProductsRoutes: Route[] = [
+  { path: '', component: FeatureProductsComponent },
+];
