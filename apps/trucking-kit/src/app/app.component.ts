192371f308ea1f9d12edddd331ab7a2a6efae074
Will Davis <wallace41290@gmail.com>
2024-06-28 16:48:02 -0400

    feat(trucking-kit): wip add nav rail

--- apps/trucking-kit/src/app/app.component.ts
+++ apps/trucking-kit/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components';
 import { AsyncPipe } from '@angular/common';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import {
   AmplifyAuthenticatorModule,
   AuthenticatorService,
 } from '@aws-amplify/ui-angular';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '@shared/auth/data-access';
-import { TkLogoModule } from '@shared/ui';
+import { TkLogoModule, TkNavigationRailComponent } from '@shared/ui';
 
 @Component({
   standalone: true,
@@ -18,7 +19,9 @@ import { TkLogoModule } from '@shared/ui';
     NavbarComponent,
     RouterModule,
     TkLogoModule,
+    TkNavigationRailComponent,
     MatButtonModule,
+    MatIconModule,
   ],
   selector: 'tk-root',
   templateUrl: './app.component.html',
@@ -29,6 +32,10 @@ export class AppComponent {
   authenticator = inject(AuthenticatorService);
 
   $isLoggedIn = this.authService.isAuthenticatedState$;
+
+  constructor(private matIconReg: MatIconRegistry) {
+    this.matIconReg.setDefaultFontSetClass('material-symbols-outlined');
+  }
 
   logout() {
     this.authService.logout();
