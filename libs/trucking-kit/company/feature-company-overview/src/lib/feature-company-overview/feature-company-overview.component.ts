7993054d5fa3ae21ff2d38656c4958c9c0c5208f
Chase Moore <pcm08@hotmail.com>
2024-08-07 21:28:32 -0400

    chore: adding setup for company overview page

--- libs/trucking-kit/company/feature-company-overview/src/lib/feature-company-overview/feature-company-overview.component.ts
+++ libs/trucking-kit/company/feature-company-overview/src/lib/feature-company-overview/feature-company-overview.component.ts
@@ -1,12 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule, KeyValuePipe } from '@angular/common';
+import { CompanyStore } from '@trucking-kit/company/data-access';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'trucking-kit-feature-company-overview',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    KeyValuePipe,
+    MatButtonModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    FormsModule,
+  ],
   templateUrl: './feature-company-overview.component.html',
   styleUrl: './feature-company-overview.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FeatureCompanyOverviewComponent {}
+export class FeatureCompanyOverviewComponent {
+  store = inject(CompanyStore);
+
+  update(form: NgForm) {
+    console.table(form.value);
+  }
+}
--- libs/trucking-kit/company/feature-company/src/lib/feature-company.component.ts
+++ libs/trucking-kit/company/feature-company/src/lib/feature-company.component.ts
@@ -1,13 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CompanyStore } from '@trucking-kit/company/data-access';
+import { take } from 'rxjs';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'tk-feature-company',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, MatDividerModule, RouterModule],
   templateUrl: './feature-company.component.html',
   styleUrl: './feature-company.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FeatureCompanyComponent {}
+export class FeatureCompanyComponent implements OnInit {
+  store = inject(CompanyStore);
+  route = inject(ActivatedRoute);
+
+  ngOnInit() {
+    this.route.data.pipe(take(1)).subscribe((data) => {
+      const dotNumber = data['dotNumber'];
+      if (dotNumber) {
+        this.store.get(dotNumber);
+      } else {
+        this.store.fetch();
+      }
+    });
+  }
+}
