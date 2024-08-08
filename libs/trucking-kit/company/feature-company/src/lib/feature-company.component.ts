b3fab1e8b08e3b5baf44fc5a2a3c5a23ccbd96f6
Will Davis <wallace41290@gmail.com>
2024-08-07 21:12:44 -0400

    feat(company): wip: page layout

--- libs/trucking-kit/company/feature-company/src/lib/feature-company.component.ts
+++ libs/trucking-kit/company/feature-company/src/lib/feature-company.component.ts
@@ -1,13 +1,30 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'tk-feature-company',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, RouterModule],
   templateUrl: './feature-company.component.html',
   styleUrl: './feature-company.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FeatureCompanyComponent {}
+export class FeatureCompanyComponent {
+  /**
+   * Layout
+   * - heading
+   * - tabs/left nav
+   * - content
+   *    - tables (drivers & trucks)
+   *
+   * Tabs
+   * - info, drivers, trucks
+   *
+   * heading
+   * - style in box
+   *
+   */
+}
